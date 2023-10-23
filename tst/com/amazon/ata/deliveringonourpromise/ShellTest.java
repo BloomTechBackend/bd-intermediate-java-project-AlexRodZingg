7f747b0c2f8475430c1710a82dcdb8b4d22b96e9
Alexander Rodriguez-Zingg <alexrodriguezzingg@icloud.com>
2023-10-23 17:58:04 -0600

    Began test set up for OrderDaoTest

--- tst/com/amazon/ata/deliveringonourpromise/ShellTest.java
+++ tst/com/amazon/ata/deliveringonourpromise/ShellTest.java
@@ -286,31 +286,6 @@ public class ShellTest {
                    "Expected result to match regex " + pattern + "result was: " + result);
     }
 
-    // Mastery Task 1 Test
-    @Test
-    public void handleUserRequest_invalidOrderId_printsUNKNOWN_ORDER_MESSAGE() {
-        // Declare the dummyOrderId
-        String dummyOrderId = "111-749023-7630574";
-        String UNKNOWN_ORDER_MESSAGE_TEST = "Unable to find any order data for orderId: %s. Please check your order id and try again.";
-
-        // Mock the PromiseHistoryClient to return null
-        PromiseHistoryClient mockClient = Mockito.mock(PromiseHistoryClient.class);
-        when(mockClient.getPromiseHistoryByOrderId(Mockito.anyString())).thenReturn(null);
-
-        // Mock the ATAUserHandler to return the dummyOrderId
-        ATAUserHandler mockUserHandler = Mockito.mock(ATAUserHandler.class);
-        when(mockUserHandler.getString(Mockito.anyString(), Mockito.anyString())).thenReturn(dummyOrderId);
-
-        // Instantiate the Shell with the mocked objects
-        Shell shell = new Shell(mockClient, mockUserHandler);
-
-        // Call handleUserRequest and check the result
-        String result = shell.handleUserRequest();
-
-        // Assertion to check that the unknown order message is returned
-        assertEquals(String.format(UNKNOWN_ORDER_MESSAGE_TEST, dummyOrderId), result, "Expected the unknown order message to be displayed for orderId: " + dummyOrderId);
-    }
-
     // FIXME: Adding some required tests for testing purposes
     @Test
     void ARequiredTest() {
--- tst/com/amazon/ata/deliveringonourpromise/dao/OrderDaoTest.java
+++ tst/com/amazon/ata/deliveringonourpromise/dao/OrderDaoTest.java
@@ -1,7 +1,31 @@
 package com.amazon.ata.deliveringonourpromise.dao;
 
-public class OrderDaoTest {
+import com.amazon.ata.deliveringonourpromise.ordermanipulationauthority.OrderManipulationAuthorityClient;
+import com.amazon.ata.deliveringonourpromise.types.Order;
+import com.amazon.ata.ordermanipulationauthority.OrderResult;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-    private OrderDao dao;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+
+public class OrderDaoTest {
+//    private OrderManipulationAuthorityClient mockClient;
+    private OrderManipulationAuthorityClient omaClient;
+    private ReadOnlyDao<String, Order> orderDao;
+
+    @Test
+    public void get_forKnownOrderId_returnsOrder() {
+        // GIVEN
+        String knownOrderId = "111-7499023-7630574";
+        // WHEN
+        Order order = orderDao.get(knownOrderId);
+
+        // THEN
+        assertNotNull(order);
+
+    }
+
+
 
 }
